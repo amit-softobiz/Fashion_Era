4ab7783fec84184aeae22414b4e391422c30a534
Amit Kumar <amit.thakur@softobiz.com>
2023-08-02 16:50:16 +0530

    optimize home page and get data from db.json

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,26 +3,20 @@ import DealOfTheWeek from "@/components/DealOfTheWeek";
 import ProductCard from "@/components/ProductCard";
 import Banner from "@/components/banner";
 import axios from "axios";
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-
-
-// Example JSON data for products
-
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default async function HomePage() {
   const productsData = await axios.get("http://localhost:4000/productss");
-   const slides = [
-    <div key={1}>
-      
-      <img src="https://media.istockphoto.com/id/506317910/photo/beautiful-little-girl-model-wearing-a-leopard-dress-and-sunglass.jpg?s=1024x1024&w=is&k=20&c=AYlxOvgD6IS6z-dCtAKLJh2qefNI1XJkPG52TS1Pa8g=" alt="Slide 1" />
-    </div>,
-        <div key={2}>
-      
-        <img src="https://img.freepik.com/free-photo/full-length-portrait-happy-excited-woman-bright-colorful-clothes-holding-shopping-bags-while-standing-showing-peace-gesture-isolated-green-wall_231208-2678.jpg?w=740&t=st=1690612793~exp=1690613393~hmac=2e9f010480de8bee6686f6d2ddd035fe7c3f54d6365f5aef064d3f73540d39c1" alt="Slide 1" />
-      </div>,
-    // Add more slides here
-  ];
+  const slidesData = await axios.get("http://localhost:4000/slides");
+  const banner = await axios.get("http://localhost:4000/banner");
+
+  const slides = slidesData.data.map((slide, index) => (
+    <div key={index}>
+      <img src={slide.img} alt={slide.title} />
+    </div>
+  ));
+
   return (
     <>
       <Carousel
@@ -32,61 +26,76 @@ export default async function HomePage() {
         showThumbs={false}
         showArrows={false}
         interval={3000}
-        className="w-2/5 mx-auto mt-16"
+        className="h-600"
       >
         {slides}
       </Carousel>
+      
       <div className="flex flex-wrap justify-center">
-      <Banner imageSrc="https://preview.colorlib.com/theme/fashi/img/banner-1.jpg.webp" title="Men" />
-      <Banner imageSrc="https://preview.colorlib.com/theme/fashi/img/banner-2.jpg.webp" title="Women" />
-      <Banner imageSrc="https://preview.colorlib.com/theme/fashi/img/banner-3.jpg.webp" title="Kids" />
-    </div>
+      {banner.data.map((bannerItem, index) => (
+          <Banner
+          imageSrc={bannerItem.img}
+          title={bannerItem.title}
+        />
+        ))}
+      </div>
 
-    <section className="women-banner spad">
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-lg-3">
-            <div className="product-large set-bg" style={{ backgroundImage: "url('https://cdn.shopify.com/s/files/1/0491/3204/4455/products/BJ0A2700_360x.jpg?v=1665779449')" }}>
-              <h2>Women’s</h2>
-              <a href="#">Discover More</a>
+      <section className="women-banner spad">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-3">
+              <div
+                className="product-large set-bg"
+                style={{
+                  backgroundImage:
+                    "url('https://cdn.shopify.com/s/files/1/0491/3204/4455/products/BJ0A2700_360x.jpg?v=1665779449')",
+                }}
+              >
+                <h2>Women’s</h2>
+                <a href="#">Discover More</a>
+              </div>
             </div>
-          </div>
-          <div className="col-lg-8 offset-lg-1">
-            <div className="filter-control">
-              <ul>
-                <li className="active">Clothings</li>
-                <li>HandBag</li>
-                <li>Shoes</li>
-                <li>Accessories</li>
-              </ul>
-            </div>
-            <div className="product-slider owl-carousel owl-loaded owl-drag">
-              {/* Owl carousel items go here */}
+            <div className="col-lg-8 offset-lg-1">
+              <div className="filter-control">
+                <ul>
+                  <li className="active">Clothings</li>
+                  <li>HandBag</li>
+                  <li>Shoes</li>
+                  <li>Accessories</li>
+                </ul>
+              </div>
+              <div className="product-slider owl-carousel owl-loaded owl-drag">
+                {/* Owl carousel items go here */}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
       <div className="text-center mt-8">
         <h1 className="text-3xl font-bold">Welcome to Our eCommerce Website</h1>
-        <p className="text-lg">Explore our amazing products and start shopping!</p>
-        <a href="/products" className="shop-now-btn mt-4 inline-block px-6 py-3 text-white bg-pink-500 rounded-lg text-lg hover:bg-pink-600 transition duration-300">
+        <p className="text-lg">
+          Explore our amazing products and start shopping!
+        </p>
+        <a
+          href="/products"
+          className="shop-now-btn mt-4 inline-block px-6 py-3 text-white bg-pink-500 rounded-lg text-lg hover:bg-pink-600 transition duration-300"
+        >
           Shop Now
         </a>
       </div>
-    
-      <div className="flex flex-wrap justify-center">
-      {productsData.data.map((product, index) => (
-        <ProductCard
-          key={index}
-          name={product.name}
-          image={product.image}
-          price={product.price}
-        />
-      ))}
-    </div>
 
-    <DealOfTheWeek/>
+      <div className="flex flex-wrap justify-center">
+        {productsData.data.map((product, index) => (
+          <ProductCard
+            key={index}
+            name={product.name}
+            image={product.image}
+            price={product.price}
+          />
+        ))}
+      </div>
+
+      <DealOfTheWeek />
     </>
   );
 }
---
+++ src/interfaces.ts
@@ -0,0 +1,15 @@
+export interface Slide {
+  img: string;
+  title: string;
+}
+
+export interface BannerItem {
+  img: string;
+  title: string;
+}
+
+export interface Product {
+  name: string;
+  image: string;
+  price: number;
+}
